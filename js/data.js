80a3418a6ce8457c62cb646df408795c0bdcd301
developer-prosenjit <bnprosenjit@gmail.com>
2013-09-14 01:24:36 +0530

    demo-1
    Basic of handelbar.js ongoing.... really awesome and nice.

---
+++ js/data.js
@@ -0,0 +1,13 @@
+// JavaScript Document
+ var data = { users: [
+      {username: "alan", firstName: "Alan", lastName: "Johnson", email: "alan@test.com" },
+      {username: "allison", firstName: "Allison", lastName: "House", email: "allison@test.com" },
+      {username: "ryan", firstName: "Ryan", lastName: "Carson", email: "ryan@test.com" }
+    ]};
+var pageTitle = {title:[
+{id:1,text:'title 1'},
+{id:2,text:'Title 2'}
+]}
+var images = {imgdata:[
+	{name:'git',url:'http://www.justinleegrant.com/wp-content/uploads/2013/07/Git-Icon-Black.png'}
+	]}
